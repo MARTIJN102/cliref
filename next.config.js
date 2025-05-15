6fd4113ee893a380373eee2fc5eb02addfe369e5
MARTIJN102 <martijnmiedema01@gmail.com>
2025-05-15 03:41:01 +0200

    .

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js'
+  themeConfig: './theme.config.js',
+  defaultShowCopyCode: true
 })
 
 module.exports = withNextra()
